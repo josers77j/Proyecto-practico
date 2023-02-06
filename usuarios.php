2b7b7ca4398b069750a1b99abcb4d6e9cfe12b78
josers77j <josers77.1@gmail.com>
2023-02-06 16:56:07 -0600

    mod

--- usuarios.php
+++ usuarios.php
@@ -86,8 +86,8 @@
 
         </div>
         <div class="modal-footer">
-          <button type="submit" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="submit" class="btn btn-primary">Send message</button>
+          <button type="submit" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+          <button type="submit" class="btn btn-primary">Guardar</button>
           </form>
         </div>
       </div>
